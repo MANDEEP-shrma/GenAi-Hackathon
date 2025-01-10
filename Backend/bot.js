9dfe39e2f86bed0253774aef74637856fa0fd3c0
Mandeep Sharma <mandeepsharma0701@gmail.com>
2025-01-10 11:10:42 +0530

    Application token change

--- Backend/bot.js
+++ Backend/bot.js
@@ -138,8 +138,7 @@ async function main(
 ) {
   const flowIdOrName = "37941ca5-0ed3-4642-873f-5d179bf3244d";
   const langflowId = "e0b69fd6-af76-4a00-b361-20fd2926e36a";
-  const applicationToken =
-    "AstraCS:kXzZWKBZulxIEMxNmckHpPpl:1c922838b3974267497b9731aa14af8d994a83eac7c48dad42614ee48249fdc8";
+  const applicationToken = process.env.APPLICATION_URL;
   const langflowClient = new LangflowClient(
     "https://api.langflow.astra.datastax.com",
     applicationToken
